65286e5c84a8050ffc30920b02d61e1dd85a8dd5
Matheus Silva <msilvaqs@outlook.com>
2024-09-24 13:51:00 -0400

    Modal and api call for invite link

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 
 import CreateServerModal from "@/components/modals/CreateServerModal";
+import InviteModal from "@/components/modals/InviteModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -15,7 +16,12 @@ const ModalProvider = () => {
     return null;
   }
 
-  return <CreateServerModal />;
+  return (
+    <>
+      <CreateServerModal />;
+      <InviteModal />
+    </>
+  );
 };
 
 export default ModalProvider;
--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -21,6 +21,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useModalStore } from "@/hooks/useModalStore";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -28,8 +29,11 @@ interface ServerHeaderProps {
 }
 
 const ServerHeader: React.FC<ServerHeaderProps> = ({ server, role }) => {
+  const { onOpen } = useModalStore();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none" asChild>
@@ -40,7 +44,10 @@ const ServerHeader: React.FC<ServerHeaderProps> = ({ server, role }) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
             Invite People
             <UserPlus className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
