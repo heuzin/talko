d484e0f02bb85bcda66a15becc471a58dc469cb5
Matheus Silva <msilvaqs@outlook.com>
2024-09-26 15:28:47 -0400

    Creating Chat header component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+import ChatHeader from "@/components/chat/ChatHeader";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage: React.FC<ChannelIdPageProps> = async ({
+  params: { channelId, serverId },
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) return auth().redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: { id: channelId },
+  });
+
+  const member = await db.member.findFirst({
+    where: { serverId: serverId, profileId: profile.id },
+  });
+
+  if (!channel || !member) return redirect("/");
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
